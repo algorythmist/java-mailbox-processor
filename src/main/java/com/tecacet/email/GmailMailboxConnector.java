e048417f6315bf345e6fd3809ec4815647af597c
algorythmist <dimitri@tecacet.com>
2019-08-09 14:27:49 -0700

    fixed gmail connection using imap

--- src/main/java/com/tecacet/email/GmailMailboxConnector.java
+++ src/main/java/com/tecacet/email/GmailMailboxConnector.java
@@ -3,17 +3,16 @@ package com.tecacet.email;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Store;
-import javax.mail.URLName;
 import java.util.Properties;
 
 /**
- * MailboxConnector configured for gmail
+ * MailboxConnector configured for gmail using IMAP
  */
 public class GmailMailboxConnector extends AbstractMailboxConnector {
 
-    private static final String DEFAULT_PROTOCOL = "pop3";
-    private static final String GMAIL_POP3_HOST = "pop.gmail.com";
-    private static final int GMAIL_POP3_PORT = 995;
+
+    private static final String IMAP_PROTOCOL = "imaps";
+    private static final String GMAIL_IMAP_HOST = "imap.gmail.com";
 
     public GmailMailboxConnector(String username, String password) {
         super(username, password, null);
@@ -21,12 +20,11 @@ public class GmailMailboxConnector extends AbstractMailboxConnector {
 
     @Override
     protected Store connect(String username, String password, Properties properties) throws MessagingException {
-        properties.put("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        properties.put("mail.pop3.socketFactory.fallback", false);
-        URLName url = new URLName(DEFAULT_PROTOCOL, GMAIL_POP3_HOST, GMAIL_POP3_PORT, "", username, password);
+        properties.setProperty("mail.store.protocol", IMAP_PROTOCOL);
+
         Session session = Session.getInstance(properties, null);
-        Store store = session.getStore(url);
-        store.connect();
+        Store store = session.getStore(IMAP_PROTOCOL);
+        store.connect(GMAIL_IMAP_HOST, username, password);
         return store;
     }
 }
